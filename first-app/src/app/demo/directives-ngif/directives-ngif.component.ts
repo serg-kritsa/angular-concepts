0da0454e3f9c16950bc013cc0dcb60920e4fa9ff
Serg <serg@example.com>
2021-11-04 02:36:17 +0300

    OPTIONAL: ngIf details

--- first-app/src/app/demo/directives-ngif/directives-ngif.component.ts
+++ first-app/src/app/demo/directives-ngif/directives-ngif.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
     <p>directives-ngif works!</p>
     <button class="btn btn-primary" (click)="clickHandler()">SHOW/HIDE</button>
     <p *ngIf="isAddedToDOM">Simple Paragraph</p>
+    <hr>
+    <!--*ngIf will be eexcuted as code below-->
+    <!--<ng-template [ngIf]="!isAddedToDOM">-->
+      <!--<p>Simple Paragraph</p>-->
+    <!--</ng-template>-->
+    <hr>
   `,
 })
 export class DirectivesNgifComponent {
@@ -14,3 +20,22 @@ export class DirectivesNgifComponent {
     this.isAddedToDOM = !this.isAddedToDOM;
   }
 }
+//// OPTIONAL: ngIf behind the scene
+// <div *myNgIf="someProp">
+// <li></li>
+// </div>
+
+// import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+// @Directive({ selector: '[myNgIf]' })
+// export class myNgIfDirective {
+//   @Input() set myNgIf(condition: boolean) {
+//     if (condition) {
+//       this.vcRef.createEmbeddedView(this.templateRef);
+//     } else {
+//       this.vcRef.clear();
+//     }
+//   }
+
+//   constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+// }
