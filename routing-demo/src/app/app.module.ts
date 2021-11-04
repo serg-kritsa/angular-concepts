f3bb3b105a850e1f12457b8f565b206a5d5446d7
Serg <serg@example.com>
2021-11-05 02:23:13 +0300

    added not-found route w/ component

--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
 import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
@@ -22,6 +24,7 @@ const appRoutes: Routes = [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ] },
+  { path: 'not-found', component: PageNotFoundComponent },
 ];
 
 @NgModule({
@@ -32,7 +35,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
