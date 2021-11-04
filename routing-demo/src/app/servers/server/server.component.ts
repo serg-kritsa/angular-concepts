60c29970c7d851ec32a6a17ef676838028746298
Serg <serg@example.com>
2021-11-05 01:37:58 +0300

    added servers/:id route support

--- routing-demo/src/app/servers/server/server.component.ts
+++ routing-demo/src/app/servers/server/server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { ServersService } from '../servers.service';
 
@@ -9,11 +11,25 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
+  paramsSubscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
+  }
+  
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
+
