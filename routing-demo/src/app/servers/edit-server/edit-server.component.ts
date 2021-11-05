39464d2fe0afd60e8fa8c4cc5b02b128130f083d
Serg <serg@example.com>
2021-11-05 15:39:39 +0300

    added update server feature

--- routing-demo/src/app/servers/edit-server/edit-server.component.ts
+++ routing-demo/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,7 +16,8 @@ export class EditServerComponent implements OnInit {
   changesSaved = false;
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -29,10 +30,17 @@ export class EditServerComponent implements OnInit {
         }
       );
     this.route.fragment.subscribe();
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    // Subscribe route params to update the id if params change
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    // this.router.navigate(['../'], {relativeTo: this.route}); 
+    this.router.navigate(['../../'], {relativeTo: this.route}); // workaround to force user to use link from /servers 
   }
 
 }
