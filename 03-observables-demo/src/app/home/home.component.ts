7821ccda5c81499287fce3879abcc3c90ff040ec
Serg <serg@example.com>
2021-11-05 18:01:11 +0300

    added more operator

--- 03-observables-demo/src/app/home/home.component.ts
+++ 03-observables-demo/src/app/home/home.component.ts
@@ -35,6 +35,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 
     this.firstObsSubscription = customIntervalObservable.pipe(
+      filter(data => {return data > 1;}), 
       map((data: number) => {return 'Round: ' + data;})
     ).subscribe(data => {
       console.log(data);
