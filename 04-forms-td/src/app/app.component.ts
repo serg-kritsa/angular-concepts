1cd8d32331e9d6308dbe60cd155d5eee05fbc48a
Serg <serg@example.com>
2021-11-05 22:57:57 +0300

    added default control value

--- 04-forms-td/src/app/app.component.ts
+++ 04-forms-td/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultSecretQuestionType = 'pet'
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
