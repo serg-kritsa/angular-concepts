1ef64b749b18ef9eb350d6c36c30442b3392509b
Serg <serg@example.com>
2021-11-06 16:56:36 +0300

    combining data w/ id

--- 05-http-demo/src/app/app.component.ts
+++ 05-http-demo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -41,7 +42,19 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get('https://ng-complete-guide-c56d3.firebaseio.com/posts.json')
+      .get('https://fir-app-99824-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      // was {"key": { ...document data  }}
+      .pipe(
+        map(responseData => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )// became { ...document_data, id: "key"}
       .subscribe(posts => {
         // ...
         console.log(posts);
