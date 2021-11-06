e2662cae33dd9573343840c7d51304c4e4848c47
Serg <serg@example.com>
2021-11-06 21:53:40 +0300

    catchError & throwError operators

--- 05-http-demo/src/app/post.service.ts
+++ 05-http-demo/src/app/post.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from "rxjs";
+import { catchError, map } from 'rxjs/operators';
 
 import { Post } from "./post.model";
 
@@ -36,6 +36,9 @@ export class PostService {
           }
           // became { ...document_data, id: "key"}
           return postsArray;
+        }),
+        catchError(errorRes => {
+          return throwError(errorRes);
         })
       )
   }
