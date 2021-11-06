a2cd22b8fc053943ee772903df4bde53056392d3
Serg <serg@example.com>
2021-11-06 03:41:50 +0300

    added validation error messages

--- 04-forms-reactive/src/app/app.component.ts
+++ 04-forms-reactive/src/app/app.component.ts
@@ -12,11 +12,12 @@ export class AppComponent implements OnInit {
   usernameControl: FormControl
   emailControl: FormControl
   hobbyControls: AbstractControl[]
+  forbiddenUsernames = ['user', 'test']
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -35,5 +36,12 @@ export class AppComponent implements OnInit {
     const hobbyFormArray = this.signupForm.get('hobbies') as FormArray
     const control = new FormControl(null, Validators.required);
     hobbyFormArray.push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null; // if successful
   }
 }
