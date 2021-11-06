5f56ce7eb5887aa2f29a3fc25faa4c269019ebee
Serg <serg@example.com>
2021-11-06 16:48:22 +0300

    requesting data from database

--- 05-http-demo/src/app/app.component.ts
+++ 05-http-demo/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -30,9 +32,19 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get('https://ng-complete-guide-c56d3.firebaseio.com/posts.json')
+      .subscribe(posts => {
+        // ...
+        console.log(posts);
+      });
   }
 }
