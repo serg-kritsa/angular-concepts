afd4f93f939c911cf04e64b761d523efa6f63c43
Serg <serg@example.com>
2021-11-06 19:14:01 +0300

    added unsubscription of Subject

--- 05-http-demo/src/app/app.component.ts
+++ 05-http-demo/src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Post } from "./post.model";
 import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSubscription: Subscription;
   
   constructor(private http: HttpClient, private postService: PostService) {}
   
   ngOnInit() {
-    this.postService.errorEmitter.subscribe(errMsg => this.error = errMsg)
+    this.errorSubscription = this.postService.errorEmitter.subscribe(errMsg => this.error = errMsg)
     this.fetchPosts();
   }
 
@@ -50,5 +52,9 @@ export class AppComponent implements OnInit {
     }, 
     error => this.postService.errorEmitter.next(error.message)
     );
+  }
+
+  ngOnDestroy(): void {
+    this.errorSubscription.unsubscribe();
   }
 }
