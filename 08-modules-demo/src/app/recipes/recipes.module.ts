d32c9d64b2f0b8e9f0f382dece859a9d782c4f08
Serg <serg@example.com>
2021-11-07 21:47:26 +0300

    added related routes in separate module

--- 08-modules-demo/src/app/recipes/recipes.module.ts
+++ 08-modules-demo/src/app/recipes/recipes.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,10 +20,16 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  //        router-outlet
-  //                      ngIf, ngFor
-  //                                    FormGroup
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  imports: [
+    // router-outlet
+    RouterModule, 
+    // ngIf, ngFor
+    CommonModule, 
+    // FormGroup
+    ReactiveFormsModule, 
+    // routes
+    RecipesRoutingModule 
+  ],
   exports: [
     RecipesComponent,
     RecipeListComponent,
