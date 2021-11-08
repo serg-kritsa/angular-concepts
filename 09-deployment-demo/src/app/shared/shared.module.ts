5a96cf67f935e03b4b827b6580a018274b7fd413
Serg <serg@example.com>
2021-11-08 17:52:22 +0300

    deployment init demo

---
+++ 09-deployment-demo/src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlertComponent } from './alert/alert.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropdownDirective } from './dropdown.directive';
+import { LoggingService } from '../logging.service';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective
+  ],
+  imports: [CommonModule],
+  // gives access to them from outside where imported
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule
+  ],
+  // Angular 9+ adds dynamic component automatically
+  // entryComponents: [AlertComponent]
+  // providers: [LoggingService]
+})
+export class SharedModule {}
