d1b1771dcb9d88c97d06cae380a52f5859d75e3b
Serg <serg@example.com>
2021-11-13 02:11:54 +0300

    added save all in db

--- 10-ngrx-demo/src/app/recipes/store/recipe.effects.ts
+++ 10-ngrx-demo/src/app/recipes/store/recipe.effects.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, map } from 'rxjs/operators';
+import { switchMap, map, withLatestFrom } from 'rxjs/operators';
 
 import * as RecipesActions from './recipe.actions';
 import { Recipe } from '../recipe.model';
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
+  // // saving only added
+  // @Effect({ dispatch: false })
+  // saveRecipes = this.actions$.pipe(
+  //   ofType(RecipesActions.ADD_RECIPE),
+  //   switchMap((data: RecipesActions.AddRecipe) => {
+  //     return this.http
+  //       .post<Recipe>(
+  //         'https://fir-app-99824-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+  //         {
+  //           name: data.payload.name,
+  //           description: data.payload.description,
+  //           imagePath: data.payload.imagePath,
+  //           ingredients: data.payload.ingredients
+  //         }
+  //       )
+  //   })
+  // );
+
   @Effect()
   fetchRecipes = this.actions$.pipe(
     ofType(RecipesActions.FETCH_RECIPES),
@@ -31,22 +51,21 @@ export class RecipeEffects {
     })
   );
 
-  @Effect({ dispatch: false })
-  saveRecipes = this.actions$.pipe(
-    ofType(RecipesActions.ADD_RECIPE),
-    switchMap((data: RecipesActions.AddRecipe) => {
-      return this.http
-        .post<Recipe>(
-          'https://fir-app-99824-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-          {
-            name: data.payload.name,
-            description: data.payload.description,
-            imagePath: data.payload.imagePath,
-            ingredients: data.payload.ingredients
-          }
-        )
+  @Effect({dispatch: false})
+  storeRecipes = this.actions$.pipe(
+    ofType(RecipesActions.STORE_RECIPES),
+    withLatestFrom(this.store.select('recipes')),
+    switchMap(([actionData, recipesState]) => {
+      return this.http.put(
+        'https://fir-app-99824-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipesState.recipes
+      );
     })
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private store: Store<fromApp.AppState>
+  ) {}
 }
